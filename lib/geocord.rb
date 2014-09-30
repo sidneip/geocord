61e56fed5de64fee292f0a9177dd895e7d18fb57
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2014-09-30 18:14:27 -0300

    return mutiples address

--- lib/geocord.rb
+++ lib/geocord.rb
@@ -4,12 +4,13 @@ require 'uri'
 require 'json'
 
 module Geocord
+	GOOGLEAPIS = 'http://maps.googleapis.com/maps/api/geocode/json?address='
 	class Cordinate
   	attr_accessor :latitude, :longitude
 	end
 
 	def self.process(address)
-		uri = URI.encode("http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=false")
+		uri = URI.encode("#{GOOGLEAPIS}#{address}&sensor=false")
 		uri = URI.parse(uri)
 		conn = Faraday.new(:url => uri)
 		response = conn.get
@@ -17,9 +18,10 @@ module Geocord
 		if data['results'].size < 1
 			return nil
 		else
-			cordinate = Cordinate.new
-			cordinate.latitude  = data['results'][0]['geometry']['location']['lat']
-			cordinate.longitude = data['results'][0]['geometry']['location']['lng'] 
+			cordinate = Array.new
+				data['results'].each_with_index do |cord, index|
+					cordinate[index] = {:endereco => cord['formatted_address'], :latitude => cord['geometry']['location']['lat'], :longitude => cord['geometry']['location']['lng']}
+				end
 			return cordinate
 		end
 	end
